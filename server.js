3097ce5f7004e9c4aac474b1d98da55dbcfff4f5
TommyHong <ToomyHong1913@outlook.com>
2024-03-29 20:35:48 +0900

    add multer pack

--- server.js
+++ server.js
@@ -2,28 +2,40 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes")
-
+const authRoutes = require("./routes/authRoutes");
+const port = process.env.PORT || 5000;
 // init express node app
 const app = express();
 
 // allow the app to use middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
-app.use("/", authRoutes)
+  console.log(req.path, req.method);
+  next();
+});
+app.use("/", authRoutes);
+
+//====================Middleware to parse JSON and urlencoded form data====================
+const upload = multer({ dest: "uploads/" });
+//====================Route to handle form submission====================
+app.post("/upload", upload.single("file"), (req, res) => {
+  // const {name, email} = req.body;
+  const file = req.file;
+  console.log("Received form data:", req.body);
+  res.send("Form data received successfully.");
+});
 
 // connect the app to a database
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log("connected to db and server started on", process.env.PORT);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log("connected to db and server started on", port);
     });
-})
-.catch((error) => {
-    console.log(error)
-})
+  })
+  .catch((error) => {
+    console.log(error);
+  });
